b64d10a03f1d0b785fe89c48563535c8caea0543
Kishan Bhut <kish.bhut@gmail.com>
2019-03-21 18:19:13 -0400

    shmoood

--- openfile/pkl/open_pkl.py
+++ openfile/pkl/open_pkl.py
@@ -1,11 +1,15 @@
 import pickle
 import os
+import numpy
 
+
+data = []
 if os.path.getsize('C3_train.pkl') > 0:
     with open('C3_train.pkl', 'rb') as f:
+        file = open("output_pkl.txt",'w')
         data = pickle.load(f)
-        file = open("testfile.txt",'w')
-        file.write(str(data))
+        print(len(data))
         print(data)
+        file.write(str(data))
 else:
     print('empty file')
